347d7ab2021d53bf1fdb2f50d348da726ba682f6
Ti7801 <tiagodaltro19@gmail.com>
2024-04-04 11:10:19 -0300

    Criando a Controller UsuarioController e a classe Usuario.

---
+++ Usuario.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuarioApi.Models;
+
+namespace UsuarioApi.Controllers
+{
+    [ApiController] // devido a esse atributo no parametro da Action eu posso passar um tipo complexo
+    [Route("[controller]")] //
+    public class UsuarioController : ControllerBase
+    {
+
+        [HttpPost]  //O FromBody não é obrigatório a partir do C# 3.1 para parametros complexos na Action!
+        public ActionResult<Usuario> CriarUsuario([FromQuery] Usuario value)
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+
+            Usuario usuario = new Usuario();
+
+            usuario.Id = value.Id;
+            usuario.Nome = value.Nome;
+            usuario.Cpf = value.Cpf;
+            usuario.Email = value.Email;
+            usuario.senha = value.senha;
+
+            usuarios.Add(usuario);
+
+            return Ok(usuario);
+        }
+    }
+}
---
+++ Usuario.Api/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace UsuarioApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string  Email { get; set; }  
+        public string senha { get; set; }
+
+
+    }
+}
