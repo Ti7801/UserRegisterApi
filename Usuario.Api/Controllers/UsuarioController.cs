1f182d176cf0d9be086c11c7059105a2af55c8d7
Ti7801 <tiagodaltro19@gmail.com>
2024-04-05 14:15:16 -0300

    Adicionando as Actions BuscarUsuario e DeletarUsuario. E atribuindo os atributos na Action CriarUsuario.

--- Usuario.Api/Controllers/UsuarioController.cs
+++ Usuario.Api/Controllers/UsuarioController.cs
@@ -4,7 +4,7 @@ using UsuarioApi.Models;
 namespace UsuarioApi.Controllers
 {
     [ApiController] // devido a esse atributo no parametro da Action eu posso passar um tipo complexo
-    [Route("[controller]")] //
+    [Route("[controller]")] 
     public class UsuarioController : ControllerBase 
     {
         private readonly List<Usuario> usuarios; // O readonly vai obrigar  a não poder alterar o usuario?
@@ -12,10 +12,12 @@ namespace UsuarioApi.Controllers
         public UsuarioController() 
         {
             usuarios = new List<Usuario>();
-        }  
+        }
 
         [HttpPost]  //O FromBody não é obrigatório a partir do C# 3.1 para parametros complexos na Action!
-        public ActionResult<Usuario> CriarUsuario([FromQuery] Usuario value) // Trocar pra [FromBody] depois
+        [ProducesResponseType(typeof(Usuario), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Usuario> CriarUsuario([FromBody] Usuario value) 
         {
             Usuario usuario = new Usuario();
 
@@ -25,10 +27,9 @@ namespace UsuarioApi.Controllers
             usuario.Email = value.Email;
             usuario.senha = value.senha;
             
-
             usuarios.Add(usuario);
 
-            return Ok(usuario);
+            return CreatedAtAction(actionName:nameof(CriarUsuario), usuario);
         }
 
         [HttpGet]
@@ -38,7 +39,7 @@ namespace UsuarioApi.Controllers
         }
 
         [HttpPut]
-        public ActionResult<Usuario> AtualizaUsuario([FromQuery] Usuario value) // Trocar pra [FromBody] depois
+        public ActionResult<Usuario> AtualizaUsuario([FromBody] Usuario value) 
         {
             var usuario = usuarios.Where(usuario => usuario.Id.Equals(value.Id)).SingleOrDefault(); 
 
@@ -55,6 +56,33 @@ namespace UsuarioApi.Controllers
         
             return usuario;
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Usuario> BuscaUsuario([FromBody] int id)
+        {
+            var usuario = usuarios.Where(usuario => usuario.Id.Equals(id)).SingleOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<List<Usuario>> DeletarUsuario([FromBody]int id) 
+        {
+            var usuario = usuarios.Where(usuario => usuario.Id.Equals(id)).SingleOrDefault();
+
+            usuarios.Remove(usuario);
+
+            return Ok(usuarios);
+        }
+
+
+
+
 
 
 
