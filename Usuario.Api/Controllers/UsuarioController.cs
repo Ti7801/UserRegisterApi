00e86e171e171a5ff4cb9071c91d395133f37864
Ti7801 <tiagodaltro19@gmail.com>
2024-04-05 09:09:16 -0300

    Adicionando a Action para listar os usuários e adicionando também a Action de Atualizar usuarios.

--- Usuario.Api/Controllers/UsuarioController.cs
+++ Usuario.Api/Controllers/UsuarioController.cs
@@ -5,14 +5,18 @@ namespace UsuarioApi.Controllers
 {
     [ApiController] // devido a esse atributo no parametro da Action eu posso passar um tipo complexo
     [Route("[controller]")] //
-    public class UsuarioController : ControllerBase
+    public class UsuarioController : ControllerBase 
     {
+        private readonly List<Usuario> usuarios; // O readonly vai obrigar  a não poder alterar o usuario?
+
+        public UsuarioController() 
+        {
+            usuarios = new List<Usuario>();
+        }  
 
         [HttpPost]  //O FromBody não é obrigatório a partir do C# 3.1 para parametros complexos na Action!
-        public ActionResult<Usuario> CriarUsuario([FromQuery] Usuario value)
+        public ActionResult<Usuario> CriarUsuario([FromQuery] Usuario value) // Trocar pra [FromBody] depois
         {
-            List<Usuario> usuarios = new List<Usuario>();
-
             Usuario usuario = new Usuario();
 
             usuario.Id = value.Id;
@@ -20,10 +24,42 @@ namespace UsuarioApi.Controllers
             usuario.Cpf = value.Cpf;
             usuario.Email = value.Email;
             usuario.senha = value.senha;
+            
 
             usuarios.Add(usuario);
 
             return Ok(usuario);
         }
+
+        [HttpGet]
+        public ActionResult<List<Usuario>> ListagemUsuarios() 
+        {
+            return Ok(usuarios);
+        }
+
+        [HttpPut]
+        public ActionResult<Usuario> AtualizaUsuario([FromQuery] Usuario value) // Trocar pra [FromBody] depois
+        {
+            var usuario = usuarios.Where(usuario => usuario.Id.Equals(value.Id)).SingleOrDefault(); 
+
+            if(usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioNovo = new Usuario()
+            {
+                Id = value.Id,
+                Nome = value.Nome,
+                Cpf = value.Cpf,
+                Email = value.Email,
+                senha = value.senha
+            };
+
+            return usuarioNovo;
+        }
+
+
+
     }
 }
--- Usuario.Api/Models/Usuario.cs
+++ Usuario.Api/Models/Usuario.cs
@@ -2,10 +2,10 @@
 {
     public class Usuario
     {
-        public int Id { get; set; } 
+        public int Id { get;  set; } //Se eu colocar private no set, ele não deixa nem eu modificar a primeira vez
         public string Nome { get; set; }
-        public string Cpf { get; set; }
-        public string  Email { get; set; }  
+        public string Cpf { get;  set; } // ou seja vai está vazio e eu não posso botar valor algum
+        public string  Email { get; set; }
         public string senha { get; set; }
 
 
